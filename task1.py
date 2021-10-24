ca92a92435e8b2c23aaeb4b6aac430c1fd1729db
Елена Маколова <92180705+ElenaMak1@users.noreply.github.com>
2021-10-24 17:40:52 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,6 +0,0 @@
-def calculator(a, b):
-    try:
-        return a/b
-    except ZeroDivisionError as e:
-        print(f'Ошибка! Делить на ноль нельзя')
-print(calculator(int(input('Первое число: ')), int(input('Второе число: '))))
